a97420d0a3da3e8bd3c14e6195a73b8735c99875
delvedor <tommydelved@gmail.com>
2018-07-31 23:17:18 +0200

    Updated cli

---
+++ cli.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const commist = require('commist')();
+const help = require('help-me')({
+  // the default
+  dir: path.join(path.dirname(require.main.filename), 'help')
+});
+
+commist.register('init', require('./lib/init'));
+commist.register('serve', require('./lib/serve'));
+commist.register('compose', require('./lib/compose'));
+commist.register('offline', require('./lib/offline'));
+commist.register('help', help.toStdout);
+commist.register('version', function() {
+  console.log(require('./package.json').version);
+});
+
+const res = commist.parse(process.argv.splice(2));
+
+if (res) {
+  // no command was recognized
+  help.toStdout(res);
+}
