40d2fd906768704c32f7f5f4aa3bbd9498d7a362
delvedor <tommydelved@gmail.com>
2018-08-12 11:57:23 +0200

    Updated lib, use standard

--- cli.js
+++ cli.js
@@ -1,27 +1,27 @@
 #! /usr/bin/env node
 
-'use strict';
+'use strict'
 
-const path = require('path');
-const commist = require('commist')();
+const path = require('path')
+const commist = require('commist')()
 const help = require('help-me')({
   // the default
   dir: path.join(path.dirname(require.main.filename), 'help')
-});
+})
 
-commist.register('init', require('./lib/init'));
-commist.register('serve', require('./lib/serve'));
-commist.register('compose', require('./lib/compose'));
-commist.register('offline', require('./lib/offline'));
-commist.register('pdf', require('./lib/pdf'));
-commist.register('help', help.toStdout);
-commist.register('version', function() {
-  console.log(require('./package.json').version);
-});
+commist.register('init', require('./lib/init'))
+commist.register('serve', require('./lib/serve'))
+commist.register('compose', require('./lib/compose'))
+commist.register('offline', require('./lib/offline'))
+commist.register('pdf', require('./lib/pdf'))
+commist.register('help', help.toStdout)
+commist.register('version', function () {
+  console.log(require('./package.json').version)
+})
 
-const res = commist.parse(process.argv.splice(2));
+const res = commist.parse(process.argv.splice(2))
 
 if (res) {
   // no command was recognized
-  help.toStdout(res);
+  help.toStdout(res)
 }
